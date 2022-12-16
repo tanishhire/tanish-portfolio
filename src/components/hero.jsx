e38821256ee3f05577c768ecd4b2da0536beb834
Tanish Hire <83215038+tanishhire@users.noreply.github.com>
2022-12-16 20:19:01 +0530

    Update hero.jsx

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -16,9 +16,9 @@ const Hero = () => {
         <h2 className='text-5xl sm:text-7xl font-bold text-[#d6d6d6]'>I am a <Typed className="pl-2" strings={['web-desginer', 'web-developer','Blogger', 'UI/UX Desginer', 'Photographer', 'Mechanical Engineer']} typeSpeed={100}
             backSpeed={120}
             loop /> </h2>
-        <p className='text-[#B0D8BC] py-2 max-w-[700px]'>Web Desginer-Developer | Aspiring Web 3 Developer | Blogger | UI/UX | Photographer | Mechanical Engineer |</p>
-        <div>
-        <a href='https://tanishhire.hashnode.dev/' target="_blank" rel="noopener noreferrer"><button className='text-white group border-2 btn-1 px-6 py-3 my-2 flex items-center justify-center 
+        <p className='text-[#B0D8BC] py-2 max-w-[700px]'>I am a web/graphic designer and developer. I write about tech, books and travel. Follow me on <a className='underline' href='https://bit.ly/twittertanish' target="_blank" rel="noopener noreferrer">twitter</a> and <a className='underline' href='https://tanishhire.hashnode.dev/' target="_blank" rel="noopener noreferrer"> hashnode. </a> </p>
+        <div className='w-1/2'>
+        <a href='https://tanishhire.hashnode.dev/' target="_blank" rel="noopener noreferrer"><button className='text-white group border-2  btn-1 px-6 py-3 my-2 flex items-center justify-center 
         rounded-md hover:bg-[#56c3e6] hover:border-[#d6d6d6] hover:scale-105'>Check out my blogs 
           <span className=' duration-300'> 
              <HiArrowNarrowRight className='ml-3' />
